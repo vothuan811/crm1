099cf60481bd112a641a5fe9561dc6ed30c709d8
vothuan811 <vothuan808@gmail.com>
2022-04-24 12:37:48 +0700

    Database Relationships

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,12 @@ class Customer(models.Model):
 	def __str__(self):
 		return self.name
 
+class Tag(models.Model):
+	name = models.CharField(max_length=200, null=True)
+	def __str__(self):
+		return self.name
+
+
 class Product(models.Model):
 	CATEGORY = (
 			('Indoor', 'Indoor'),
@@ -20,6 +26,8 @@ class Product(models.Model):
 	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
 	description = models.CharField(max_length=200, null=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	tags = models.ManyToManyField(Tag)
+
 
 
 class Order(models.Model):
@@ -29,7 +37,7 @@ class Order(models.Model):
 			('Delivered', 'Delivered'),
 			)
 
-	#customer = 
-	#product = 
+	customer = models.ForeignKey(Customer, null=True, on_delete = models.SET_NULL)
+	product = models.ForeignKey(Product, null=True, on_delete = models.SET_NULL)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	status = models.CharField(max_length=200, null=True, choices=STATUS)
